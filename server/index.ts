947ed836f2c6351e06505e82c434b94bb5e41a8d
Hung SST <metaron23>
2025-11-30 10:14:41 +0700

    update api payment

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ import { handleLogin } from "./routes/login";
 import { handleRegister } from "./routes/register";
 import { handleForgetPass, handleResetPassword } from "./routes/forget-pass";
 import { getAllActiveMoviesToday } from "./routes/movies";
+import { createPayment, updatePayment } from "./routes/payment";
 
 
 export function createServer() {
@@ -29,6 +30,9 @@ export function createServer() {
   app.post("/api/forget-password", handleForgetPass);
   app.post("/api/reset-password", handleResetPassword);
   app.post("/api/getActiveMovies", getAllActiveMoviesToday);
+  app.post("/api/create-booking", createPayment); // sử dụng để tạo đặt vé sau khi ấn nút thanh toán
+  app.post("/api/confirm-booking", updatePayment); // sử dụng để xử lý data do momo trả về sau khi người dùng thanh toán thành công
+  
 
   return app;
 }
