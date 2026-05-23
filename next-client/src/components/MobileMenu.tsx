e1a0cbfbee23f940472b031aa06409d26f41418e
baonhatt <baonha20@gmail.com>
2026-05-23 11:49:20 +0700

    [fix] update validate form booking

--- next-client/src/components/MobileMenu.tsx
+++ next-client/src/components/MobileMenu.tsx
@@ -29,7 +29,7 @@ export function MobileMenu({
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="default" className="bg-opacity-20 border-white/20 text-white w-10 h-10">
+        <Button variant="default" className="bg-white/10 backdrop-blur-md border-white/20 text-white w-10 h-10 hover:bg-white/20">
           <Menu className="h-6 w-6" />
         </Button>
       </SheetTrigger>
@@ -47,7 +47,7 @@ export function MobileMenu({
               <img
                 src="/icon.svg"
                 alt="CINESPHERE"
-                className="h-16 w-16 cursor-pointer drop-shadow-[0_0_20px_rgba(59,130,246,0.5)]"
+                className="h-20 w-20 cursor-pointer drop-shadow-[0_0_20px_rgba(59,130,246,0.5)]"
               />
             </div>
 
